53397cffaf43157cf79856ae1bfcdf03b58d9ac9
mauro <mauropanella89@gmail.com>
2018-01-17 18:25:23 -0300

    agrego el pase de pagina 2 a 3

--- src/pages/pagina2/pagina2.ts
+++ src/pages/pagina2/pagina2.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import {Pagina3Page} from "./../pagina3/pagina3"
 /**
  * Generated class for the Pagina2Page page.
  *
@@ -15,7 +16,23 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class Pagina2Page {
 
+  mutantes:any[]=[{
+    nombre:"magneto",
+    poder:"controla metales"},
+    {nombre:"wolverine",
+    poder:"regeneracion"},
+    {nombre:"profesor x",
+    poder:"poderes psiquicos"},
+    {nombre:"gambito",
+    poder:"carga de energia objetos"}
+  ];
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  irPagina3(mutante:any){
+    console.log(mutante);
+    this.navCtrl.push(Pagina3Page,{'mutante':mutante});
   }
 
   ionViewDidLoad() {
